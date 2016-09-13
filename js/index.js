6ab092ccc05cf0a80037bb74185000f28360ec23
LauraAilor <lauragraceailor@gmail.com>
2016-09-13 15:05:04 -0400

    Changed access to bootstrap and updated javascrips
    Bootstrap is now accessed through maxCDN, javascript file updated to
    meet features, and changed style

---
+++ js/index.js
@@ -0,0 +1,29 @@
+
+var food = [];
+
+function displayPantry() {
+    var text = "", i;
+    for (i = 0; i < food.length; i++) {
+        text += food[i];
+        if (i + 1 !== food.length) {
+            text += ", ";
+        }
+    }
+    document.getElementById("list").innerHTML = text;
+}
+
+function addToList() {
+    var newFood = document.getElementById("input").value;
+    food.push(newFood);
+    document.getElementById("input").value = "";
+    displayPantry();
+}
+
+function clearPantry() {
+    food = [];
+    displayPantry();
+}
+
+function findRecipe() {
+    alert("There are no recipes that match your ingredients!");
+}
